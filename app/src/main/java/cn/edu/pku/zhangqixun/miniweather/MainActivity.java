9e261e6d02db7e6be573348cb3b13327ee582cf5
dongyacong <1253959316@qq.com>
2018-10-07 23:20:01 +0800

    hello

---
+++ app/src/main/java/cn/edu/pku/zhangqixun/miniweather/MainActivity.java
@@ -0,0 +1,16 @@
+package cn.edu.pku.zhangqixun.miniweather;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Administrator on 2018/10/7.
+ */
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.weather_info);
+    }
+}
